5fec9bb953b22c9b1d421d9f12f58cdec38cf3c1
FaroukC <farouk.channouf@maibornwolff.de>
2022-01-10 23:49:29 +0100

    feat: add Cart component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
 import { Product, products } from './../products';
 
 @Component({
@@ -9,11 +10,19 @@ import { Product, products } from './../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
     const routerParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routerParams.get('productId'));
     this.product = products.find(({ id }) => id === productIdFromRoute);
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert(`The product ${product.name} has been added to your cart!`);
   }
 }
