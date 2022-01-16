08076e55c61c27d1c2f3568878bfbb40a955b31e
FaroukC <farouk.chn@gmail.com>
2022-01-16 22:51:01 +0100

    feat: add Shipping route and link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { TopBarComponent } from './top-bar/top-bar.component'
 import { ProductListComponent } from './product-list/product-list.component'
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component'
 import { ProductDetailsComponent } from './product-details/product-details.component'
-import { CartComponent } from './cart/cart.component';
+import { CartComponent } from './cart/cart.component'
 import { ShippingComponent } from './shipping/shipping.component'
 
 @NgModule({
@@ -21,6 +21,7 @@ import { ShippingComponent } from './shipping/shipping.component'
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ]),
   ],
   declarations: [
